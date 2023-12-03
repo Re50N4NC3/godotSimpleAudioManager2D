55aa8a8ba020304f7757ae8589e9555bf6f0bc95
Re50N4NC3 <dmnkchdl@gmail.com>
2023-12-03 01:40:33 +0100

    Bug fixes

--- audio_manager/audio_manager.gd
+++ audio_manager/audio_manager.gd
@@ -1,12 +1,12 @@
 extends Node
 
-var sound_component: PackedScene = load("res://audio_manager/component_sound.gd")
+var sound_component: PackedScene = load("res://audio_manager/component_sound_player.tscn")
 @export var sounds_list: Array[ComponentSoundResource] = []
 var sounds_names: Array[String] = []
 
 func _ready():
 	for snd_no in range(0, sounds_list.size()):
-		sounds_names[snd_no] = sounds_list[snd_no].sound_name
+		sounds_names.append(sounds_list[snd_no].sound_name)
 
 func play_sound(sound_name: String, position: Vector2):
 	var sound_to_play: AudioStreamMP3 = find_sound_resource(sound_name)
@@ -15,7 +15,7 @@ func play_sound(sound_name: String, position: Vector2):
 		var snd: ComponentSoundPlayer = sound_component.instantiate()
 		snd.global_position = position
 		snd.sound_file = sound_to_play
-		snd.get_parent().add_child.call_deferred(snd)
+		get_parent().add_child.call_deferred(snd)
 	else:
 		print("Couldn't find sound with name " + sound_name + " in Sounds List")
 
