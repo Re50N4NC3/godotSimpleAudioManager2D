22c7fbed4f77fa329edf1a1e7ca076b09b94a024
Re50N4NC3 <dmnkchdl@gmail.com>
2023-12-03 02:15:11 +0100

    refactored manager to include position of sound

--- audio_manager/audio_manager.gd
+++ audio_manager/audio_manager.gd
@@ -1,25 +1,40 @@
 extends Node
 
 var sound_component: PackedScene = load("res://audio_manager/component_sound_player.tscn")
-@export var sounds_list: Array[ComponentSoundResource] = []
+@export var sounds_resource_list: Array[ComponentSoundResource] = []
 var sounds_names: Array[String] = []
 
 func _ready():
-	for snd_no in range(0, sounds_list.size()):
-		sounds_names.append(sounds_list[snd_no].sound_name)
+	for snd_no in range(0, sounds_resource_list.size()):
+		sounds_names.append(sounds_resource_list[snd_no].sound_name)
 
 func play_sound(sound_name: String, position: Vector2):
-	var sound_to_play: AudioStreamMP3 = find_sound_resource(sound_name)
+	var sound_position = _find_sound_resource_position(sound_name)
 	
-	if sound_to_play != null:
+	if sound_position != -1:
+		var sound_to_play: AudioStreamMP3 = sounds_resource_list[sound_position].sound_file
 		var snd: ComponentSoundPlayer = sound_component.instantiate()
+		
 		snd.global_position = position
 		snd.sound_file = sound_to_play
+		snd.volume_change_range = Vector2(sounds_resource_list[sound_position].volume_change_min, 
+										  sounds_resource_list[sound_position].volume_change_max)
+		
 		get_parent().add_child.call_deferred(snd)
 	else:
-		print("Couldn't find sound with name " + sound_name + " in Sounds List")
+		print("Couldn't find sound with called name `" + sound_name + "` in Sounds List")
 
-func find_sound_resource(sound_name: String) -> AudioStreamMP3:
+func _find_sound_resource_position(sound_name: String) -> int:
 	if sound_name in sounds_names:
-		return sounds_list[sounds_names.find(sound_name, 0)].sound_file
-	return null
+		var sound_position: int = sounds_names.find(sound_name, 0)
+		return sound_position
+	return -1
+
+func stop_all_sounds():
+	pass
+
+func resume_all_sounds():
+	pass
+	
+func remove_all_sounds():
+	pass
--- audio_manager/component_sound_player.gd
+++ audio_manager/component_sound_player.gd
@@ -3,10 +3,18 @@ class_name ComponentSoundPlayer
 
 var audio_player: AudioStreamPlayer2D
 var sound_file: AudioStreamMP3
+var volume_change_range: Vector2
+var pitch_change_range: Vector2
+var max_distance: int
+var area_mask: int
 
 func _ready():
 	audio_player = get_node("AudioPlayer")
 	audio_player.stream = sound_file
+	audio_player.volume_db = randf_range(volume_change_range[0], volume_change_range[1])
+	audio_player.pitch_scale = randf_range(pitch_change_range[0], pitch_change_range[1])
+	audio_player.max_distance = max_distance
+	audio_player.area_mask = area_mask
 	audio_player.play()
 	audio_player.finished.connect(remove_stream)
 	
