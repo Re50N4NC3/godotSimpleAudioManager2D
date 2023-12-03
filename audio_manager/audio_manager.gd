878aabadd1e89b64b87fce04080f5f738fbdb24e
Re50N4NC3 <dmnkchdl@gmail.com>
2023-12-03 02:34:58 +0100

    Refactored naming and split into functions

--- audio_manager/audio_manager.gd
+++ audio_manager/audio_manager.gd
@@ -9,16 +9,15 @@ func _ready():
 		sounds_names.append(sounds_resource_list[snd_no].sound_name)
 
 func play_sound(sound_name: String, position: Vector2):
-	var sound_position = _find_sound_resource_position(sound_name)
+	var sound_position_in_array = _find_sound_resource_position(sound_name)
 	
-	if sound_position != -1:
-		var sound_to_play: AudioStreamMP3 = sounds_resource_list[sound_position].sound_file
+	if sound_position_in_array != -1:
+		var sound_to_play: AudioStreamMP3 = sounds_resource_list[sound_position_in_array].sound_file
 		var snd: ComponentSoundPlayer = sound_component.instantiate()
 		
 		snd.global_position = position
 		snd.sound_file = sound_to_play
-		snd.volume_change_range = Vector2(sounds_resource_list[sound_position].volume_change_min, 
-										  sounds_resource_list[sound_position].volume_change_max)
+		_setup_sound_values(snd, sound_position_in_array)
 		
 		get_parent().add_child.call_deferred(snd)
 	else:
@@ -26,9 +25,17 @@ func play_sound(sound_name: String, position: Vector2):
 
 func _find_sound_resource_position(sound_name: String) -> int:
 	if sound_name in sounds_names:
-		var sound_position: int = sounds_names.find(sound_name, 0)
-		return sound_position
+		var sound_position_in_array: int = sounds_names.find(sound_name, 0)
+		return sound_position_in_array
 	return -1
+	
+func _setup_sound_values(snd: ComponentSoundPlayer, sound_position_in_array: int) -> void:
+	snd.volume_change_range = Vector2(sounds_resource_list[sound_position_in_array].volume_change_min, 
+									  sounds_resource_list[sound_position_in_array].volume_change_max)
+	snd.pitch_change_range = Vector2(sounds_resource_list[sound_position_in_array].pitch_scale_min, 
+									  sounds_resource_list[sound_position_in_array].pitch_scale_max)
+	snd.max_distance = sounds_resource_list[sound_position_in_array].max_distance
+	snd.area_mask = sounds_resource_list[sound_position_in_array].area_mask
 
 func stop_all_sounds():
 	pass
